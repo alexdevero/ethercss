b70cf1ec15edff9535004c71fe96547d095eea68
Alex Devero <deveroalex@gmail.com>
2016-06-13 09:11:15 +0200

    Added Gulpfile.

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+// Gulpfile for EtherCSS
+// author Alex Devero
+
+'use strict';
+
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-ruby-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    util = require('gulp-util');
+
+// Sass to CSS
+
+gulp.task('sass', function() {
+  return sass('src/ethercss.scss', {sourcemap: true})
+    .on('error', sass.logError)
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(sourcemaps.write('.', {
+      includeContent: false,
+      sourceRoot: 'source'
+    }))
+    .pipe(gulp.dest('dist'))
+});
+
+// Sass to CSS minified
+
+gulp.task('sassMin', function() {
+  return sass('src/ethercss.scss', {sourcemap: true, style: 'compressed'})
+    .on('error', sass.logError)
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(sourcemaps.write('.', {
+      includeContent: false,
+      sourceRoot: 'source'
+    }))
+    .pipe(gulp.dest('dist'))
+});
+
+// Build EtherCSS
+
+gulp.task('build', ['sass', 'sassMin']);
